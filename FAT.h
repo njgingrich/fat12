14550852838a6123a18cc015fca32dfb37b01051
nathan <njgingrich@gmail.com>
2016-04-24 20:49:33 -0400

    added start of tests

--- FAT.h
+++ FAT.h
@@ -8,6 +8,9 @@ class FAT {
     public:
         FATDriver();
         void dir(std::string dir_name);
+        void read_sector(int num, bool is_root_dir);
+        void get_next_sector(int num, bool is_root_dir);
+        
     private:
         char* open_file(std::string filename);
 };
